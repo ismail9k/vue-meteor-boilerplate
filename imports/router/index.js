f6d966d7694b45b2eb36bb1a6e5a456c95748c6f
Ismail9k <abdelrahman@knawat.com>
2020-11-25 19:32:46 +0300

    feat: update router structure

--- imports/router/index.js
+++ imports/router/index.js
@@ -1,13 +1,4 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import routes from './routes';
-
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
-});
+import router from './router';
+import './middleware';
 
 export default router;
---
+++ imports/router/middleware/auth.js
@@ -0,0 +1,6 @@
+import router from '../router';
+
+// router gard
+router.beforeEach((to, from, next) => {
+  next();
+});
